5eb554b1436c99c93af91fc08ec0a70f2e2830c6
Miro <mr.mladenoff@gmail.com>
2022-06-03 20:45:21 -0700

    localStorage ready

--- app.js
+++ app.js
@@ -3,36 +3,53 @@ const todoList = document.querySelector("#todo-list");
 
 // retrieve from localStorage
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-
-
-todoList.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
-    e.target.parentElement.remove();
-    console.log(e);
-  } else if (e.target.tagName === "LI") {
-    e.target.classList.add("crossedToDO");
-    isCompleted 
+for (let i = 0; i < savedTodos.length; i++) {
+  let newTodo = document.createElement("li");
+  newTodo.innerText = savedTodos[i].task;
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+  if (newTodo.isCompleted) {
+    newTodo.style.textDecoration = "line-through";
   }
-});
+  todoList.appendChild(newTodo);
+}
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const todoInput = document.querySelector("#todo-input");
-  const newLi = document.createElement("li");
+  const newTodo = document.createElement("li");
+  newTodo.isCompleted = false;
   if (todoInput.value === "") {
     return '';
   }
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = " Delete";
-
-  newLi.textContent = todoInput.value;
   
-  newLi.classList.add("todo-list");
-  todoList.appendChild(newLi);
-  newLi.appendChild(deleteBtn);
+  newTodo.textContent = todoInput.value;
+  
+  newTodo.classList.add("todo-list");
+  todoList.appendChild(newTodo);
   deleteBtn.classList.add('delbtn')
   todoInput.value = '';
+  
+  savedTodos.push({ task: newTodo.innerText, isCompleted: false });
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+  
+  newTodo.appendChild(deleteBtn);
+});
+todoList.addEventListener("click", (e) => {
+  let clickedListItem = e.target;
 
-  localStorage.setItem('todo-list', JSON.stringify(todoList))
-
- });
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+    console.log(e);
+  } else if (e.target.tagName === "LI") {
+    if (!clickedListItem.isCompleted) {
+      clickedListItem.style.textDecoration = "line-through";
+      clickedListItem.isCompleted = true;
+    } else {
+      clickedListItem.style.textDecoration = "none";
+      clickedListItem.isCompleted = false;
+    }
+    
+  }
+});
